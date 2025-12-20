1634f395ac50940f11508cc4abb4b224fa6c9d12
Hadi <potern412@gmail.com>
2025-12-21 02:12:25 +0330

    initial project

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// app
+
+import './index.css';
+import React from 'react';
+import {ScrollTrigger , SplitText } from "gsap/all";
+import gsap from "gsap";
+
+gsap.registerPlugin(ScrollTrigger , SplitText);
+
+
+
+export default function App(props) {
+    return (
+        <section className={``}></section>
+    );
+}
+
+
